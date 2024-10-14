15e02839945c598abfba21a7cf19817abfd11cc4
Jewel Ghosh <jewel@Jewels-MacBook-Air.local>
2024-10-15 01:02:23 +0530

    added dialectOptions in sequelize instance creation

--- db/db.js
+++ db/db.js
@@ -3,10 +3,15 @@ require('dotenv').config();//{path:'../.env'}
 const Sequelize = require('sequelize');
 
 //Option:1 using Passing parameter separately
-const sequelizeInstance=new Sequelize(`${process.env.DB_NAME}`,`${process.env.DB_USER_NAME}`,`${process.env.DB_PASSWORD}`,
-{
+const sequelizeInstance=new Sequelize(`${process.env.DB_NAME}`,`${process.env.DB_USER_NAME}`,`${process.env.DB_PASSWORD}`,{
     host:process.env.DB_HOST,
-    dialect:process.env.DB_DIALECT
+    dialect:process.env.DB_DIALECT,
+    dialectOptions: {
+      ssl: {
+        require: true, // This is required for AWS RDS PostgreSQL
+        rejectUnauthorized: false // This can be set to true if you have the necessary certificates
+      }
+    }
 });
 
 // option 2: Using Connection URL(For Render)
